579ce7da9f2b219c81e95fcf746255521d729e0f
K-Kanchan <91278349+K-Kanchan@users.noreply.github.com>
2022-03-03 20:14:47 +0530

    Add files via upload

---
+++ tick cross game/Tick Cross Game.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+	cout<<"\n\n Tick Cross Game"<<endl;
+	cout<<"Player1[X]\nPlayer2 [0]\n\n";
+	
+	cout<<"\t\t     |     |     \n";
+	cout<<"\t\t  1  | 2   | 3   \n";
+	cout<<"\t\t_____|_____|_____\n";
+	cout<<"\t\t     |     |     \n";
+	cout<<"\t\t 4   |  5  |  6  \n";
+	cout<<"\t\t____ |_____|_____\n";
+	cout<<"\t\t     |     |     \n";
+	cout<<"\t\t 7   |  8  |  9  \n";
+	cout<<"\t\t     |     |     \n";
+	cout<<"\n\tPlayer1 [X] turn:";
+	
+}
+
